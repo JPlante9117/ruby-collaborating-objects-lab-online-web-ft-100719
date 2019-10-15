8baad14194d49ed7332f308cad163b6162cb34eb
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 17:21:39 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,8 +10,8 @@ class MP3Importer
     Dir["#{path}/*.mp3"].map { |file| file.split("/").last }
   end
   
-  def import(list_of_files)
-    list_of_files.each { |filename| Song.new_by_filename(filename) }
+  def import
+    files.each { |filename| Song.new_by_filename(filename) }
   end
   
 end
