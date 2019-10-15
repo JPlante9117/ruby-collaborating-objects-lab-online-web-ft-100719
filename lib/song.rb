3ccd899574b7b3c9af9c5bfcc56dc6b0b00cfd90
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 17:56:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,9 @@ class Song
   end
   
   def self.new_by_filename(file_name)
+     = file_name.gsub(/(.mp3)/, "").split(" - ")
     song = self.new
-    song.name = file_name.gsub(/(.mp3)/, "").split(" - ")[1]
+    song.name = 
     song
   end
   
