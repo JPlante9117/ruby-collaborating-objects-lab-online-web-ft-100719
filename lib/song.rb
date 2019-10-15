919c71f5107296525d293ddde7a99baad8d65d97
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 18:27:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,7 @@ class Song
   def self.new_by_filename(file_name)
     artist_song_genre = file_name.gsub(/(.mp3)/, "").split(" - ")
     song = self.new(artist_song_genre[1])
+    song
     artist = Artist.find_or_create_by_name(artist_song_genre[0])
     artist.add_song(song)
   end
