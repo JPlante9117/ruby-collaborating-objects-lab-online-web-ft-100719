bdd3c8489c39c4cbc790778451b83d5f318ee395
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 17:49:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(given_name)
-    if @@all.select{|artist| artist.name == given_name}
+    if @@all.select{|artist| artist.name == given_name}[0]
       @@all.select{|artist| artist.name == given_name}[0]
     else
        artist = artist.new
